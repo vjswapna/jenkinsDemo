af76a6eb7df6420ad9b33eeab22d1ebe75f51938
vjswapna <129140069+vjswapna@users.noreply.github.com>
2023-03-28 23:34:34 -0500

    My second upload for practice testing Jenkins

---
+++ practice.py
@@ -0,0 +1,56 @@
+a="hello world"
+c=a 
+print(c)
+
+#def test_mymethod():
+    #print('hello from my method')
+    #assert 1==1
+a = [10,20,30]
+for i in a:
+    print("in loop")
+else:
+    print("in else block")
+
+
+
+    str1="welcome"
+    str2="hello"
+    print(id(str1),id(str2))
+
+    
+
+    def getname():
+     return "swapna"
+print(getname())
+
+
+a=[1,2,3,4,5]
+print(a[2])
+
+a=[1,2,3,4,5]
+print(a)
+num=','.join(str(i)for i in a)
+print(num)
+
+a=[1,2,3,4]
+b=[5,6,7]
+a.extend(b)
+print(a)
+
+
+
+
+
+names=['swapna','ammu','anish' ,'ayodh']
+anames=[name for name in names if name [0]=='a']
+print(anames)
+
+names="swapnaammuanishayodh"
+print(set(names))
+print(len(names))
+
+
+
+
+
+
